d96a9f3b8536f910b3e5dd204e80931c0ba155d8
Kasper Wind <kasperwind79@gmail.com>
2023-05-03 23:02:09 +0200

    Added mongodb and test code

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,29 @@
 import express, {Express, Request, Response} from 'express'
 import dotenv from 'dotenv'
+import { MongoClient } from 'mongodb'
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
+const uri = process.env.MONGODB
 
-app.use(express.static('public'))
+
+async function run() {
+    const client = new MongoClient(uri!)
+
+    const db = client.db("sample-data")
+    const supplies = db.collection("sample_suppliers")
+
+    const query = { storeLocation: "Denver"}
+    const options = {}
+
+    const sale = await supplies.findOne(query, options)
+    console.log(sale)
+}
+
+run().catch(console.dir)
+app.use(express.static('./dist/public'))
 
 app.get('/mjello', (req: Request, res: Response) => {
     res.send('Mjello ')
